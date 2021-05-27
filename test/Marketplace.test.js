97106bae238487e1252423f293ccef6a4fde5981
Eddine Omar <eddine.farid@hotmail.fr>
2021-05-27 11:15:29 +0200

    update smartcontract

--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -7,33 +7,28 @@ const SimpleAuction = contract.fromArtifact('SimpleAuction'); // Loads a compile
 const ether = 10 ** 18; // 1 ether = 1000000000000000000 wei
 const [owner, alice, bob] = accounts;
 
-//const OurBank = contract.fromArtifact('OurBank');
 describe("SimpleAuction", () => {
-
-    beforeEach( async () => {
+    beforeEach(async () => {
         this.current_time = new Date(Date.now());
         console.log(this.current_time, this.current_time.getTime());
-    }
-
-    )
+    })
 
     it("should create a NFT", async () => {
-        const mySimpleAuction = await SimpleAuction.new("name", "symb", {from: owner});
-        await mySimpleAuction.safeMint(owner, "tokenURI.json", {from: owner});
-        
+        const mySimpleAuction = await SimpleAuction.new("name", "symb", { from: owner });
+        await mySimpleAuction.safeMint(owner, "tokenURI.json", { from: owner });
+
         assert.equal((await mySimpleAuction.totalSupply()).toString(), "1");
         assert.equal((await mySimpleAuction.balanceOf(owner)).toString(), "1");
         assert.equal((await mySimpleAuction.ownerOf(0)).toString(), owner);
     });
 
     it("should create an auction", async () => {
-        current_time = new Date(Date.now());
-        const mySimpleAuction = await SimpleAuction.new("name", "symb", {from: owner});
-        await mySimpleAuction.safeMint(owner, "test.json", {from: owner});
+        const mySimpleAuction = await SimpleAuction.new("name", "symb", { from: owner });
+        await mySimpleAuction.safeMint(owner, "test.json", { from: owner });
 
-        await mySimpleAuction.createAuction(0, "auction test", "", 1, current_time.getTime().toString(), {from: owner})
-        await mySimpleAuction.bidOnAuction(0, {from: alice, value: 20});
-        await mySimpleAuction.bidOnAuction(0, {from: bob, value: 30});
+        await mySimpleAuction.createAuction(0, "auction test", "metadata", 1, parseInt(Date.now()), { from: owner })
+        await mySimpleAuction.bidOnAuction(0, { from: alice, value: 20 });
+        await mySimpleAuction.bidOnAuction(0, { from: bob, value: 30 });
 
         assert.equal(await mySimpleAuction.getCount(), 1);
         assert.equal(await mySimpleAuction.getBidsCount(0), 2);
@@ -41,13 +36,14 @@ describe("SimpleAuction", () => {
         console.log("list_auction_owner", list_auction_owner)
         assert.equal(list_auction_owner.length, 1);
         current_bid = await mySimpleAuction.getCurrentBid(0);
-        
+
         console.log(current_bid[0].toString(), current_bid[1])
         assert.equal(current_bid[0].toString(), "30");
         assert.equal(current_bid[1].toString(), bob);
 
         console.log((await time.latest()).toString())
-        await time.increase(300);
+        await time.increaseTo(Date.now());
+        await time.advanceBlock();
         console.log((await time.latest()).toString())
         res = await mySimpleAuction.finalizeAuction(0, {from: owner});
         console.log(res);
