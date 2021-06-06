12cf5de522792c0dabbcb48c117cfa06557435e1
Eddine Omar <eddine.farid@hotmail.fr>
2021-06-06 21:29:20 +0300

    add collection getters and setters

--- contracts/NFTCollections.sol
+++ contracts/NFTCollections.sol
@@ -7,13 +7,30 @@ contract NFTCollections {
     Counters.Counter private _collectionIds;
 
     struct NFTCollection {
-        uint256 id; //256 Collection maximum
+        uint256 id;
         string name;
         string image;
         uint256[] NFT; // NFT per collection
     }
 
     mapping(uint256 => NFTCollection) NFTCollectionMapping;
+    mapping(uint256 => uint256) NFTinCollectionMapping;
+
+    function getCollectionCount() public view returns (uint256) {
+        return _collectionIds.current();
+    }
+
+    function getCollectionInfo(uint256 _coll_id)
+        public
+        view
+        returns (NFTCollection memory)
+    {
+        return NFTCollectionMapping[_coll_id];
+    }
+
+    function getCollectionOfNFT(uint256 NFT_id) public view returns (uint256) {
+        return NFTinCollectionMapping[NFT_id];
+    }
 
     function _createCollection(string memory name, string memory image)
         internal
@@ -37,13 +54,13 @@ contract NFTCollections {
         myNFTCollection.NFT.push(_tokenId);
     }
 
-    function _removeNFTfromCollection(uint256 _tokenId, uint256 _collectionId) internal {
-        NFTCollection storage myNFTCollection =
-            NFTCollectionMapping[_collectionId];
-        for(uint i = 0; i<myNFTCollection.NFT.length; i++) {
-            if(myNFTCollection.NFT[i] == _tokenId) {
-                
-            }
-        }
-    }
+    // function _removeNFTfromCollection(uint256 _tokenId, uint256 _collectionId) internal {
+    //     NFTCollection storage myNFTCollection =
+    //         NFTCollectionMapping[_collectionId];
+    //     for(uint i = 0; i<myNFTCollection.NFT.length; i++) {
+    //         if(myNFTCollection.NFT[i] == _tokenId) {
+    //             myNFTCollection.NFT.
+    //         }
+    //     }
+    // }
 }
